f5fb3ec97b93cde66546346bd45b62e42085630a
10White <diogo08sa@gmail.com>
2023-07-07 12:24:17 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// function buttonBuy () {
+    
+//     function evento (event) {
+//         const clicar = document.querySelector(`.buy`)
+//     }
+
+//     addEventListener(`submit`, evento)
+
+//     if (evento() === true) {
+//         console.log(`clicado`)
+//     }
+//     else {
+//         console.log(`clicou na mesma`)
+//     }
+//     return
+// }
+
+// buttonBuy()
